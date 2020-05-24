79e59ca5149e088bb1c3b584b8d56f63c455c1a1
masaaki.naito <naito@lassic.co.jp>
2020-05-24 07:47:20 +0000

    for heroku

--- resources/views/commons/category_list.blade.php
+++ resources/views/commons/category_list.blade.php
@@ -1,9 +1,7 @@
 <ul class="list-group list-group-flush" style="max-width: 400px;">
     @foreach($categories as $category)
-        @if($category -> parent_category_id === 0)
-            <li class="list-group-item list-group-item-action">
-                {!! link_to_route('categories.index',  $category -> name, ['id' => $category->id]) !!}
-            </li>
-        @endif
+        <li class="list-group-item list-group-item-action">
+            {!! link_to_route('categories.index',  $category -> name, ['id' => $category->id]) !!}
+        </li>
     @endforeach
 </ul>
